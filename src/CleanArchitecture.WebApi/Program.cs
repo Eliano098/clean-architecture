ca8b33bff7c58602326fca143f7223ce8a76540e
Eliano Cordeiro <elianocordeiro18@gmail.com>
2026-08-12 20:49:28 -0300

    feat: init

---
+++ src/CleanArchitecture.WebApi/Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapGet("/health", () => Results.Ok(new { status = "healthy" }))
+    .WithName("HealthCheck");
+
+app.MapControllers();
+
+app.Run();
